af941af83093d63ec91c2441749937a5a0e07b85
Ivan <elderivanargueta04@gmail.com>
2026-09-05 12:59:02 -0700

    Still a bit confufed on enums but understanding them a bit more

--- Cargo/src/bin/practice_problems/game_event_system.rs
+++ Cargo/src/bin/practice_problems/game_event_system.rs
@@ -7,10 +7,10 @@ const MAX_HEALTH: f32 = 100.0;
 const MAX_PLAYERS: usize = 5;
 
 enum GameEvent{
-    PlayerJoined(String, i32),
+    PlayerJoined(String, i32, bool),
     PlayerMoved(String, i32, (i32, i32, i32)),
-    PlayerAttacked,
-    PLayerDied(bool),
+    PlayerAttacked(String, i32, i32),
+    PLayerDied(String, i32),
     GameEnded([Player; MAX_PLAYERS]),
 }
 
@@ -64,8 +64,9 @@ struct Player {
     username: String,
     id: i32,
     position: (i32, i32, i32),
+    is_alive: bool,
+    is_online: bool,
     character: Character,
-    is_alive: bool
 }
 
 impl Player {
@@ -74,7 +75,8 @@ impl Player {
             username,
             id: rand::thread_rng().gen_range(1..=100),
             position: (0,0,0),
-            is_alive: true,
+            is_alive: false,
+            is_online: false,
             character: Character::create_character(chosen_character),
         }
     }
@@ -87,22 +89,68 @@ impl Player {
         println!("Health: {}", self.character.get_heath());
         println!("Melee Damage: {}", self.character.get_attack_damage());
     }
+
+    fn set_online_status(&mut self) {
+        if self.is_online == false {
+            self.is_online = true
+        } else {
+            self.is_online = false
+        }
+    }
 }
 
-// fn handle_event(GameEvent)
+fn handle_event(game_event: GameEvent) {
+    match game_event {
+        PlayerJoined(username, id, mut status) => {
+            status = true;
+            println!("User: {}, with ID: {}, has joined, is alive: {}", username, id, status);
+        },
+        PlayerMoved(username, id, (x, y, z)) => {
+            println!("User: {}, with ID: {}, has moved to ({}, {}, {})", username, id, x, y, z);
+        }
+        PlayerAttacked(username, id, melee_damage) => {
+            println!("User: {}, with id: {}, has attacked with damage: {}", username, id, melee_damage);
+        }
+        PLayerDied(username, id) => {
+            println!("User: {}, with id: {}, has died", username, id);
+        }
+        GameEnded(players) => {
+            let mut active_count = MAX_PLAYERS;
+            for player in players {
+                if player.is_alive == false {
+                    active_count -= 1;
+                }
+            }
+
+            if active_count == 1 {
+                println!("Game over!");
+            }
+
+            println!("Game Still active!");
+        }
+    }
+}
 
 fn main() {
-    let player1 = Player::create_player(String::from("Ivan"), String::from("Warrior"));
-    let player2 = Player::create_player(String::from("Cricri"), String::from("Mage"));
-    let player3 = Player::create_player(String::from("Marvin"), String::from("Archer"));
-    let player4 = Player::create_player(String::from("Ammaar"), String::from("Warrior"));
-    let player5 = Player::create_player(String::from("Mike"), String::from("Archer"));
+    let mut player1 = Player::create_player(String::from("Ivan"), String::from("Warrior"));
+    let mut player2 = Player::create_player(String::from("Cricri"), String::from("Mage"));
+    let mut player3 = Player::create_player(String::from("Marvin"), String::from("Archer"));
+    let mut player4 = Player::create_player(String::from("Ammaar"), String::from("Warrior"));
+    let mut player5 = Player::create_player(String::from("Mike"), String::from("Archer"));
 
 
-    let players: [Player; MAX_PLAYERS] = [player1, player2, player3, player4, player5];
+    let players: [&mut Player; MAX_PLAYERS] = [&mut player1, &mut player2, &mut player3, &mut player4, &mut player5];
 
-    for player in  players {
-        player.print_player_stats();
-        println!();
+    for player in &players {
+        handle_event(PlayerJoined(player.username.clone(), player.id.clone(), player.is_alive.clone()));
+    }
+
+    println!();
+
+    for player in &players {
+        let x = rand::thread_rng().gen_range(-100..=100);
+        let y = rand::thread_rng().gen_range(-100..=100);
+        let z = rand::thread_rng().gen_range(-100..=100);
+        handle_event(PlayerMoved(player.username.clone(), player.id.clone(), (x, y, z)));
     }
 }
