d876c72ecbafa02737edf06af083fc288bb8c726
Ivan <elderivanargueta04@gmail.com>
2026-09-08 12:13:30 -0700

    Text game

--- Cargo/src/bin/practice_problems/text_adventure.rs
+++ Cargo/src/bin/practice_problems/text_adventure.rs
@@ -1,33 +1,297 @@
+use std::f32::consts::E;
 use std::io;
-use crate::Weapon::{Axe, Bow, Sword};
+use rand::Rng;
+use crate::Weapon::{Axe, Bow, Sword, IceThrower};
+use crate::EnemyType::{Skeleton, Goblin, Dragon};
+use crate::Action::{Up, Down, Left, Right, OpenChest, Attack, Retreat, Quit};
+const MAX_ENEMIES: usize = 5;
 
 enum Action {
-    Up,
-    Down,
-    Left,
-    Right,
-    OpenChest,
-    Attack,
-    Quit,
+    Up, // w
+    Down, // s
+    Left, // a
+    Right, // d
+    OpenChest, // e
+    Attack, // space
+    Retreat, // r
+    Quit, // q
 }
 
 enum Weapon {
     Sword(f32),
     Bow(f32, u32),
-    Axe(f32)
+    Axe(f32),
+    IceThrower(f32, u32),
 }
 
-enum Enemy {
+enum EnemyType {
     Skeleton,
     Goblin,
     Dragon,
+}
+struct Enemy {
+    enemy_type: EnemyType,
+    health: f32,
+    damage: f32,
+}
+
+impl Enemy {
+    fn create_enemy(enemy_type: EnemyType) -> Enemy {
+        let health:f32 = match enemy_type {
+            Skeleton => 100.0,
+            Goblin => 75.0,
+            Dragon => 150.0,
+        };
+
+        let damage: f32 = match enemy_type {
+            Skeleton => 10.0,
+            Goblin => 2.5,
+            Dragon => 20.0,
+        };
+
+        Enemy {
+            enemy_type,
+            health,
+            damage
+        }
+    }
+
+    fn print_enemy_stats(&self) {
+        let enemy_type:&str = match self.enemy_type {
+            Skeleton => "Skeleton",
+            Goblin => "Goblin",
+            Dragon => "Dragon",
+        };
+
+        println!("Type: {}", enemy_type);
+        println!("Health: {}", self.health);
+        println!("Damage: {}", self.damage);
+    }
+
+    fn attack(&self, player: &mut Player) {
+        match self.enemy_type {
+            Skeleton => {
+                println!("Skeleton attacked {}", player.name);
+                player.health -= self.damage;
+                if player.health <= 0.0 {
+                    println!("Game over you died");
+                    return;
+                } else {
+                    println!("Player health: {}", player.health);
+                }
+            }
+
+            Goblin => {
+                println!("Goblin attacked {}", player.name);
+                player.health -= self.damage;
+                if player.health <= 0.0 {
+                    println!("Game over you died");
+                    return;
+                } else {
+                    println!("Player health: {}", player.health);
+                }
+            }
+
+            Dragon => {
+                println!("Dragon attacked {}", player.name);
+                player.health -= self.damage;
+                if player.health <= 0.0 {
+                    println!("Game over you died");
+                    return;
+                } else {
+                    println!("Player health: {}", player.health);
+                }
+            }
+        }
+    }
 }
 struct Player {
     name: String,
     health: f32,
     weapon: Option<Weapon>,
 }
+
+impl Player {
+    fn print_player_stats(&self) {
+        let weapon_name: &str = match self.weapon {
+            Some(Sword(_)) => {"Sword"},
+            Some(Bow(_,_)) => {"Bow"},
+            Some(Axe(_)) => {"Axe"},
+            Some(IceThrower(_,_)) => {"Ice Thrower"},
+            None => {"No weapon"},
+        };
+
+
+        println!("Name: {}", self.name);
+        println!("Weapon: {}", weapon_name);
+        println!("Health: {}" ,self.health);
+    }
+
+    fn attack(&self, enemy: &mut Enemy, enemy_type: EnemyType) {
+        match enemy_type {
+            Skeleton => {
+                match self.weapon {
+                    Some(Sword(damage)) => {
+                        println!("Hit skeleton with Sword for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(Bow(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit skeleton with Bow for {} damage", damage * 0.15);
+                        enemy.health -= damage * 0.15;
+                        ammo -= 1;
+                    }
+
+                    Some(Axe(damage)) => {
+                        println!("Hit skeleton with Axe for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(IceThrower(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit skeleton with Ice Thrower for {} damage", damage * 0.25);
+                        enemy.health -= damage * 0.25;
+                        ammo -= 1;
+                    }
+
+                    None => {
+                        print!("Hit skeleton with hand for 1 damage");
+                        enemy.health -= 1.0;
+                    }
+                }
+            },
+
+            Goblin => {
+                match self.weapon {
+                    Some(Sword(damage)) => {
+                        println!("Hit goblin with Sword for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(Bow(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit goblin with Bow for {} damage", damage);
+                        enemy.health -= damage;
+                        ammo -= 1;
+                    }
+
+                    Some(Axe(damage)) => {
+                        println!("Hit goblin with Axe for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(IceThrower(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit goblin with Ice Thrower for {} damage", damage);
+                        enemy.health -= damage;
+                        ammo -= 1;
+                    }
+
+                    None => {
+                        print!("Hit goblin with hand for 1 damage");
+                        enemy.health -= 1.0;
+                    }
+                }
+            },
+
+            Dragon => {
+                match self.weapon {
+                    Some(Sword(damage)) => {
+                        println!("Hit dragon with Sword for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(Bow(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit dragon with Bow for {} damage", damage);
+                        enemy.health -= damage;
+                        ammo -= 1;
+                    }
+
+                    Some(Axe(damage)) => {
+                        println!("Hit dragon with Axe for {} damage", damage);
+                        enemy.health -= damage;
+                    }
+
+                    Some(IceThrower(damage, mut ammo)) => {
+                        if ammo == 0 {
+                            println!("Out of ammo no damage");
+                            return;
+                        }
+                        println!("Hit dragon with Ice Thrower for {} damage", damage * 2.5);
+                        enemy.health -= damage * 2.5;
+                        ammo -= 1;
+                    }
+
+                    None => {
+                        print!("Hit dragon with hand for 1 damage");
+                        enemy.health -= 1.0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn handle_player_choice(action: Action, game_active: bool, current_location: &mut usize, areas: &[&str]) {
+    match action {
+        Up | Left => {
+            if *current_location == 0 {
+                println!("You're already as far back as you can go.");
+            } else {
+                *current_location -= 1;
+                println!("You move to: {}", areas[*current_location]);
+            }
+        }
+
+        Down | Right => {
+            if *current_location == areas.len() - 1 {
+                println!("You're already as far forward as you can go.");
+            } else {
+                *current_location += 1;
+                println!("You move to: {}", areas[*current_location]);
+            }
+        }
+        
+    }
+}
+
 fn main() {
+
+    // Create array of enemies initially set to None (empty).
+    let mut enemy_array: [Option<Enemy>; MAX_ENEMIES] = [const { None }; MAX_ENEMIES];
+    let areas = ["Forest", "Broken City", "Underground cellar", "Torn down school"];
+    let game_active = true;
+
+    // Fill in enemy_array with rand generated enemies
+    for i in 0..MAX_ENEMIES {
+        let rand = rand::thread_rng().gen_range(0..3);
+        let new_enemy: Enemy = match rand {
+            0 => Enemy::create_enemy(Skeleton),
+            1 => Enemy::create_enemy(Goblin),
+            2 => Enemy::create_enemy(Dragon),
+            _ => panic!("Failed to create an enemy"),
+        };
+        enemy_array[i] = Some(new_enemy);
+    }
+
+
 
     let mut user_name = String::new();
     let mut user_weapon = String::new();
@@ -42,8 +306,9 @@ fn main() {
     io::stdin().read_line(&mut user_weapon).expect("Failed to read");
 
     let weapon_number: i32 = user_weapon.trim().parse().expect("Not a number");
+    user_name = String::from(user_name.trim());
 
-    let player = Player {
+    let mut player = Player {
         name: user_name,
         health: 100.0,
         weapon: match weapon_number {
@@ -54,5 +319,11 @@ fn main() {
         }
     };
 
+    while game_active {
+        let rand_enemy_index = rand::thread_rng().gen_range(0..MAX_ENEMIES);
+        let Some(enemy) = enemy_array[rand_enemy_index];
 
+
+
+    }
 }
