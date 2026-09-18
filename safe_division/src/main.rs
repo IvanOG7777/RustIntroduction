2baad4c19d7120ef15e52683ee4df58a08ea98e9
Ivan <elderivanargueta04@gmail.com>
2026-09-18 11:08:59 -0700

    Parse error fix and new probelm

--- safe_division/src/main.rs
+++ safe_division/src/main.rs
@@ -1,3 +1,4 @@
+use std::num::ParseFloatError;
 fn divide(a: f64, b: f64) -> Result<f64, String> {
     if b == 0.0 {
         return Result::Err(String::from("Cannot divide by 0"));
@@ -6,20 +7,31 @@ fn divide(a: f64, b: f64) -> Result<f64, String> {
     Result::Ok(a / b)
 }
 
-fn calculate(a: &str, b: &str) -> Result<f64, String> {
-    let a_as_num: f64 = a.parse()?;
-    let b_as_num: f64 = b.trim().parse().expect("B is not a number");
+fn calculate_match(a: &str, b: &str) -> Result<f64, String> {
+    let a_as_num: f64 = match a.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err(String::from("A is not a number")),
+    };
 
-    let result = divide(a_as_num, b_as_num);
+    let b_as_num: f64 = match b.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err(String::from("B is not a number")),
+    };
 
-    result
+    divide(a_as_num, b_as_num)
+}
+
+fn calculate_operator(a: &str, b: &str) -> Result<f64, String> {
+    let a_as_num: f64 = a.trim().parse().map_err(|err: ParseFloatError| err.to_string())?;
+    let b_as_num: f64 = b.trim().parse().map_err(|err: ParseFloatError| err.to_string())?;
+
+    divide(a_as_num, b_as_num)
 }
 
 fn main() {
-    let result = calculate("10", "2");
-    match result {
-        Ok(num) => println!("Total is: {num}"),
+    let result = calculate_match("2", "bb");
+    let result_1 = calculate_operator("2", "s");
 
-        Err(err) => println!("{err}")
-    }
+    println!("Total is: {result:?}");
+    println!("Total is: {result_1:?}");
 }
