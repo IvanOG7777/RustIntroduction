8203341be760b3176b67845209d9cfa68ef34469
Ivan <elderivanargueta04@gmail.com>
2026-09-22 10:23:54 -0700

    Working on library
    I honestly would die for rust enums. I LOVE THEM

--- simple_library/src/main.rs
+++ simple_library/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 struct Book {
@@ -6,15 +7,58 @@ struct Book {
     available: bool,
 }
 
+// Instance of library will own book when inserted
 struct Library {
+    // use book title as key, and book as value
     books: HashMap<String, Book>,
+}
+
+impl Book {
+    fn new_book(title: String, author: String, available: bool) -> Book {
+        Book {
+            title,
+            author,
+            available
+        }
+    }
+}
+
+// custom enum return values
+enum ReturnValues {
+    AddOk(String),
+    AddErr(String),
+
+    RemoveOk(String, Book),
+    RemoveErr(String),
+
 }
 
 impl Library {
 
-    fn add() {}
+    // new library function to create a new hashmap for instance of library
+    fn new_library() -> Library {
+        Library {
+            books: HashMap::new(),
+        }
+    }
 
-    fn remove() {}
+    fn add(&mut self, title: String, author: String) -> ReturnValues {
+        let new_book = Book::new_book(title, author.clone(), true);
+        let title = new_book.title.clone();
+        let author_ = new_book.author.clone();
+
+        self.books.insert(title.clone(), new_book);
+
+        ReturnValues::AddOk(format!("Book: {}, by {} was added", title, author_))
+    }
+
+    fn remove(&mut self, title: String) -> ReturnValues {
+        match self.books.entry(title) {
+            Entry::Occupied(book) => {
+                
+            }
+        }
+    }
 
     fn search() {}
 
@@ -26,5 +70,13 @@ impl Library {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut library = Library::new_library();
+
+    let result = library.add(String::from("Intel For Idiots"), String::from("Ivan"));
+
+    match result {
+        ReturnValues::AddOk(message) => println!("{message}"),
+
+        (_) => {}
+    }
 }
---
+++ simple_library/target/debug/simple_library.d
@@ -0,0 +1,1 @@
+C:\Users\elder\OneDrive\Desktop\RustIntroduction\simple_library\target\debug\simple_library.exe: C:\Users\elder\OneDrive\Desktop\RustIntroduction\simple_library\src\main.rs
